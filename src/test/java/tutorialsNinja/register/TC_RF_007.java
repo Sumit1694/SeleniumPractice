c0ffce24ee3452a7498f1c9f244b541171af36bb
Sumit1694 <Sumitrane198@gmail,com>
2025-07-12 00:50:15 +0530

    test cases added

---
+++ src/test/java/tutorialsNinja/register/TC_RF_007.java
@@ -0,0 +1,36 @@
+package tutorialsNinja.register;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_RF_007 {
+
+
+	@Test
+	public void verifyRegisterWithAllFields() throws InterruptedException
+	{
+	WebDriver driver=new ChromeDriver();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	driver.manage().window().maximize();
+	driver.get("https://tutorialsninja.com/demo/");
+	
+	driver.findElement(By.xpath("//span[text()='My Account']")).click();
+	driver.findElement(By.linkText("Register")).click();
+	Assert.assertTrue(driver.findElement(By.xpath("//ul[@class='breadcrumb']//a[text()='Register']")).isDisplayed());
+	
+	driver.findElement(By.xpath("//span[text()='My Account']")).click();
+	driver.findElement(By.xpath("//ul[@class='dropdown-menu dropdown-menu-right']//a[text()='Login']")).click();
+	driver.findElement(By.linkText("Continue")).click();
+	Assert.assertTrue(driver.findElement(By.xpath("//ul[@class='breadcrumb']//a[text()='Register']")).isDisplayed());
+	driver.findElement(By.xpath("//span[text()='My Account']")).click();
+	driver.findElement(By.xpath("//ul[@class='dropdown-menu dropdown-menu-right']//a[text()='Login']")).click();
+	driver.findElement(By.xpath("//aside//a[2]")).click();
+	Assert.assertTrue(driver.findElement(By.xpath("//ul[@class='breadcrumb']//a[text()='Register']")).isDisplayed());
+	driver.quit();
+	}
+}
---
+++ src/test/java/tutorialsNinja/register/TC_RF_008.java
@@ -0,0 +1,46 @@
+package tutorialsNinja.register;
+
+import java.time.Duration;
+import java.util.Date;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_RF_008 {
+
+    @Test
+    public void verifyRegisterWithAllFields() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://tutorialsninja.com/demo/");
+        
+        driver.findElement(By.xpath("//span[text()='My Account']")).click();
+        driver.findElement(By.linkText("Register")).click();
+        driver.findElement(By.id("input-firstname")).sendKeys("Sumit");
+        driver.findElement(By.id("input-lastname")).sendKeys("rane");
+        driver.findElement(By.id("input-email")).sendKeys(generateNewEmail());
+        driver.findElement(By.id("input-telephone")).sendKeys("234354657");
+        driver.findElement(By.id("input-password")).sendKeys("RaneSumit123");
+        driver.findElement(By.id("input-confirm")).sendKeys("RaneSumit1235"); // intentionally mismatched
+        driver.findElement(By.xpath("//label[@class='radio-inline']//input[@value='1']")).click();
+        driver.findElement(By.name("agree")).click();
+        driver.findElement(By.xpath("//input[@value='Continue']")).click();
+        
+        Thread.sleep(1000); // for visibility; can be avoided if wait is implemented
+        
+        // Capture and verify error message
+        String expectedErrorText = "Password confirmation does not match password!";
+        String actualErrorText = driver.findElement(By.xpath("//div[@class='col-sm-10']//div[@class='text-danger']")).getText();
+        Assert.assertEquals(actualErrorText, expectedErrorText);
+        
+        driver.quit();
+    }
+
+    public static String generateNewEmail() {
+        return new Date().toString().replaceAll(" ", "").replaceAll(":", "") + "@gmail.com";
+    }
+}
