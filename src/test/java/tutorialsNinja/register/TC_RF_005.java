35ceaee3f630d2dcfc49b301ac0784a1899116b1
Sumit1694 <Sumitrane198@gmail,com>
2025-07-12 00:13:09 +0530

    fifth test cases commit

---
+++ src/test/java/tutorialsNinja/register/TC_RF_005.java
@@ -0,0 +1,48 @@
+package tutorialsNinja.register;
+
+import java.time.Duration;
+import java.util.Date;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_RF_005 {
+
+	@Test
+	public void verifyRegisterWithAllFields() throws InterruptedException
+	{
+	WebDriver driver=new ChromeDriver();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	driver.manage().window().maximize();
+	driver.get("https://tutorialsninja.com/demo/");
+	
+	driver.findElement(By.xpath("//span[text()='My Account']")).click();
+	driver.findElement(By.linkText("Register")).click();
+	driver.findElement(By.id("input-firstname")).sendKeys("Sumit");
+	driver.findElement(By.id("input-lastname")).sendKeys("rane");
+	driver.findElement(By.id("input-email")).sendKeys(generateNewEmail());
+	driver.findElement(By.id("input-telephone")).sendKeys("234354657");
+	driver.findElement(By.id("input-password")).sendKeys("RaneSumit123");
+	driver.findElement(By.id("input-confirm")).sendKeys("RaneSumit123");
+	driver.findElement(By.xpath("//label[@class='radio-inline']//input[@value='1']")).click();
+	driver.findElement(By.name("agree")).click();
+	driver.findElement(By.xpath("//input[@value='Continue']")).click();
+	
+	Thread.sleep(3000);
+	driver.findElement(By.linkText("Continue")).click();
+	driver.findElement(By.linkText("Subscribe / unsubscribe to newsletter")).click();
+	
+	Assert.assertTrue(driver.findElement(By.xpath("//ul[@class='breadcrumb']//a[text()='Newsletter']")).isDisplayed());
+	Assert.assertTrue(driver.findElement(By.xpath("//label[@class='radio-inline']//input[@name='newsletter' and  @value='1']")).isSelected());
+	
+	driver.quit();
+	}
+	
+	public static String generateNewEmail()
+	{
+		return new Date().toString().replaceAll(" ", "").replaceAll(":", "")+"@gmail.com";
+	}
+}
